bebac4b960303538527f242c2f3632a6035d93b7
Beast Code <beastcode9999@gmail.com>
2024-02-02 21:49:15 +0930

    add boilerplate function in script

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+const Gameboard = (function Gameboard(){
+
+})();
+
+const MiniMax = (function MiniMax(){
+
+})();
+ 
+const TicTacToe = (function TicTacToe(){
+
+
+    const init = function init() {
+
+    };
+
+    return {
+        init: init
+    }
+})();
+
+window.addEventListener('load', function(){
+    TicTacToe.init();    
+})
 
 
 
-
-
+// footer script
 function addDynamicFooterDate() {
     const footer = document.querySelector('.footer__year');
     let year = new Date().getFullYear();
