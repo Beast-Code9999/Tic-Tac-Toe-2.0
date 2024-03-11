338c1ddd51c8b235f6e130c2735e8c4ae7e8a8f4
Beast Code <beastcode9999@gmail.com>
2024-03-11 18:45:35 +0930

    add init() in each gobal functions

--- script.js
+++ script.js
@@ -5,20 +5,43 @@ function getElemById(id) {
 const Gameboard = (function Gameboard(){
 
 
+    const init = function init() {
 
+    }
+    return {
+        init: init,
+    }
 })();
 
 const MiniMax = (function MiniMax(){
 
+
+    const init = function init() {
+
+    }
+    return {
+        init: init,
+    }
 })();
 
 const GameController = (function GameController(){
 
+    const init = function init() {
 
+    }
+    return {
+        init: init,
+    }
 })();
 
 const DisplayController = (function DisplayController(){
 
+    const init = function init() {
+
+    }
+    return {
+        init: init,
+    
 })();
  
 const TicTacToe = (function TicTacToe(){
